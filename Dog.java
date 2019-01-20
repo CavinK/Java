66c40fad9fc377d6cba459540ee234bb9855045a
Cavin <ykwark@gmail.com>
2019-01-20 14:43:56 +0900

    Create Dog.java

---
+++ Dog.java
@@ -0,0 +1,13 @@
+package task;
+
+public class Dog extends Animal {
+	
+	public Dog() {
+		
+	}
+	
+	public String makeSound() { // override: use methods of upper class, but in a little bit different way 
+		return "Woof";
+	}
+
+}
